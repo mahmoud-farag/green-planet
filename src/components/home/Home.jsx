033578c1890ece3a40036a1efdbc1604fdbd12f9
mahmoud-farag <mahmoud32salamn@gmail.com>
2022-02-13 14:07:06 +0200

    connect locatons to api

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React ,{useEffect, useState}from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css'
+import axios from 'axios';
+
 
 export default function Home() {
+  const [location, setLocations] = useState([]);
+   
+   const  getLocations = async()=>{
+      try {
+        const {data}= await axios.get('http://localhost:4000/api/v1/location');
+        console.log(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    
+  useEffect(()=>{
+    getLocations();
+  },[getLocations])
+
+
   return <div className='home--container'>
       {/* <svg   xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
        <path fill="#00cba9" fill-opacity="1" d="M0,256L60,218.7C120,181,240,107,360,85.3C480,64,600,96,720,101.3C840,107,960,85,1080,90.7C1200,96,1320,128,1380,144L1440,160L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path>
